686a8322ec4a3f203f4941afafa877714f9bfd2b
Richard Tom <tomrichard14@gmail.com>
2018-09-06 13:13:54 -0700

    finished required drills

--- sortDrills.js
+++ sortDrills.js
@@ -6,6 +6,9 @@ const main = () => {
   let data = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
 
   let numArray = data.split(' ').map(char => parseInt(char,10));
+  let qArray = numArray.slice();
+  let mArray = numArray.slice();
+  let bArray = numArray.slice();
 
   let test = [3,2,32,123,213];
   let qSortCounter = 0;
@@ -45,8 +48,8 @@ const main = () => {
     array[j] = tmp;
   }
   
-  // qSort(test);
-  // console.log(qSortCounter);
+  let sorted = qSort(qArray);
+  console.log('quick sort ', qSortCounter);
 
 
   //   MergeSort
@@ -92,8 +95,8 @@ const main = () => {
     return array;
   }
 
-  // console.log(mSort(numArray));
-  // console.log(mSortCounter);
+  mSort(mArray);
+  console.log('merge sort ', mSortCounter);
 
   // Bucket sort
   // Write an O(n) algorithm to sort an array of integers, where you know in advance what the lowest and highest values are.
@@ -131,16 +134,49 @@ const main = () => {
 
   }
 
-  bucketSort(numArray);
+  // bucketSort(numArray);
   
   //98+1/n 10 
   // Import some type of insertion sort to use in bucket sort function Create bucketSort function (array, bucket size) Create vars for i, min, max, and bucket size Find min and max value Create amount of buckets Push values to correct buckets Sort buckets
 
+  //testing bubble sort run time
+  let bSortCounter = 0;
+  function bubbleSort(array) {
+    
+    let swaps = 0;
+    for (let i=0; i<array.length - 1; i++) {
+      bSortCounter++;
+      if (array[i] > array[i + 1]) {
+        swap(array, i, i + 1);
+        swaps++;
+      }
+    }
+
+    if (swaps > 0) {
+      return bubbleSort(array);
+    }
+    return array;
+  }
+
+  bubbleSort(bArray);
+  console.log('bubble sort', bSortCounter);
+
 
   //   Sort in place
-// Write an algorithm to shuffle an array into a random order in-place (i.e. without creating a new array).
+// Write an algorithm to shuffle an array into a random order in-place (i.e. without creating a new array). O(n);
 
+  function random(arr) {
+    
+    for (let i = 0; i < arr.length; i++) {
+      let random = Math.floor(Math.random()*arr.length);
 
+      swap(arr, i, random);
+    }
+
+    return arr;
+  }
+
+  console.log(random(sorted));
 };
 
 main();
